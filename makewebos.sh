ea93037a59df15567e606bbce3d64a1626635ae1
Achim Königs <garfonso@tratschtante.de>
2013-11-03 23:40:45 +0100

    copy appinfo.json to deploy folder. Delete build and deploy folders before build.

--- makewebos.sh
+++ makewebos.sh
@@ -1,10 +1,11 @@
-rm -R deploy/*
+rm -rf build
+rm -rf deploy
 rm *.bar
 rm *.zip
 rm *.ipk
 ./tools/deploy.sh 
 cp *.png deploy/tt-rssenyo/
+cp appinfo.json deploy/tt-rssenyo/
 mkdir deploy/tt-rssenyo/resource
 palm-package deploy/tt-rssenyo
-
 
